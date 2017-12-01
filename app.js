89d6065df44208888d2e3a623dc0f585b8f84946
Dmitriy Blotnitskiy <dimonguitars@gmail.com>
2017-12-01 00:09:10 -0500

    app.js change

--- app.js
+++ app.js
@@ -59,5 +59,5 @@ app.get('/add-articles', function(req, res){
 
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log('Server running on default port 3000');
+  console.log('Server running on default  3000');
 });
